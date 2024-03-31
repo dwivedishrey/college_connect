14235dd7e5b4009955ee8e9da9913090b087f699
Shivangi Baranwal <shivangibaranwal15@gmail.com>
2024-03-31 19:15:53 +0530

    login page design

--- src/components/Login_page.js
+++ src/components/Login_page.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 function Login_page() {
   return (
-    <h1>login page</h1>
+    <div className="login_page">
+      <div className='login_item'>
+      <h3 className='login-head'>Connect With Peers</h3>
+      <img className="login_img" src="./img/clipart3.jpeg" />
+      </div>
+      <div className='login_item'>
+        <form className='login_form'>
+        <h3>Log In</h3>
+          <input type="email"  placeholder='  Enter Your Email' name="email"/><br/>
+          <input type='password' placeholder='  Enter Your Password' name='password' />
+         <button type='button' className='login_button'>Log in</button>
+         <hr/>
+         <div  className='small_button'>
+          <button type='button'>Google</button>
+          <button type='button'>Instagram</button>
+         </div>
+         <button type='button' className='login_button'>New user? Sign Up</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
